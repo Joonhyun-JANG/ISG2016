5e07ec24cda5e8986f74038432e1fde7ee833a76
Joonhyun JANG <joonzzang90@naver.com>
2016-11-03 22:20:37 +0900

    ISG20_20161103_2220

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ using namespace cv;
 #define WIDTH	320
 #define HEIGHT	240
 
-Mat src, src_gray, src_gray_tmp, img_scene, src_HSV, src_H, src_S, src_V, src_ball;
+Mat src, src_gray, src_gray_tmp, img_scene, src_HSV, src_H, src_S, src_V, src_ball, src_ball_gray;
 Mat milk1;
 Mat dst, detected_edges, ROI;
 
@@ -194,8 +194,8 @@ int main(int argc, char** argv) {
 						src.at<Vec3b>(i, j).val[2] = 255;
 						red_cnt[i / 40][j / 40]++;
 
-						ball_cnt_x[i]++;
-						ball_cnt_y[j]++;
+						//ball_cnt_x[i]++;
+						//ball_cnt_y[j]++;
 						src_ball.at<Vec3b>(i, j).val[0] = 0;
 						src_ball.at<Vec3b>(i, j).val[1] = 0;
 						src_ball.at<Vec3b>(i, j).val[2] = 255;
@@ -390,6 +390,7 @@ int main(int argc, char** argv) {
 
 
 void detect_ball(){
+/*
 	int i_max=0, i_max_i=0, j_max=0, j_max_i=0;
 	for(int j=0;j<WIDTH;j++){
 		if(j_max<=ball_cnt_y[j]){
@@ -412,7 +413,22 @@ void detect_ball(){
 		}
 		ball_cnt_x[i] = 0;
 	}
+*/
 
+	cvtColor(src_ball, src_ball_gray, CV_BGR2GRAY);
+	GaussianBlur(src_ball_gray, src_ball_gray, Size(9,9), 2, 2);
+
+	vector<Vec3f> circles;
+
+	HoughCircles(src_ball_gray, circles, CV_HOUGH_GRADIENT, 1, src_ball_gray.rows/8, 200, 100, 0, 0);
+
+	for(int i=0;i<circles.size();i++){
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
+
+		circle(src_ball, center, 3, Scalar(0,255,0), -1, 8, 0);
+		circle(src_ball, center, radius, Scalar(255,0,0), 3, 8, 0);
+	}
 	imshow("ball", src_ball);
 
 }
